8593022ffb5c0a6b5054119df5ab2a136a8a4940
nilhan <nejat.ilhan@gmail.com>
2013-03-08 13:22:16 +0200

    al bakalım saka

--- mobileMatematik_2/gen/com/example/mobilematematik/R.java
+++ mobileMatematik_2/gen/com/example/mobilematematik/R.java
@@ -12,26 +12,28 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icsum=0x7f020001;
     }
     public static final class id {
         public static final int TextView1=0x7f070000;
         public static final int TextView2=0x7f070001;
-        public static final int button1=0x7f070003;
-        public static final int button_Settings=0x7f070010;
-        public static final int button_StartPlaying=0x7f07000f;
-        public static final int button_add=0x7f070004;
-        public static final int button_divide=0x7f070007;
-        public static final int button_easy=0x7f070009;
-        public static final int button_medium=0x7f07000a;
-        public static final int button_multiply=0x7f070006;
-        public static final int button_sideBySide=0x7f07000d;
-        public static final int button_some_hard=0x7f07000b;
-        public static final int button_substract=0x7f070005;
-        public static final int button_topButtom=0x7f07000e;
-        public static final int button_veryEasy=0x7f070008;
-        public static final int button_very_hard=0x7f07000c;
-        public static final int editText3=0x7f070002;
-        public static final int menu_settings=0x7f070011;
+        public static final int button1=0x7f070002;
+        public static final int button_Settings=0x7f070011;
+        public static final int button_StartPlaying=0x7f070010;
+        public static final int button_add=0x7f070005;
+        public static final int button_divide=0x7f070008;
+        public static final int button_easy=0x7f07000a;
+        public static final int button_medium=0x7f07000b;
+        public static final int button_multiply=0x7f070007;
+        public static final int button_sideBySide=0x7f07000e;
+        public static final int button_some_hard=0x7f07000c;
+        public static final int button_substract=0x7f070006;
+        public static final int button_topButtom=0x7f07000f;
+        public static final int button_veryEasy=0x7f070009;
+        public static final int button_very_hard=0x7f07000d;
+        public static final int editText1=0x7f070003;
+        public static final int imageView1=0x7f070004;
+        public static final int menu_settings=0x7f070012;
     }
     public static final class layout {
         public static final int activity_blackboard=0x7f030000;
